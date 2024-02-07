00d98ec2cfe0198c724d0c616484d25cea38bf8a
Andy Lamothe-Futu <2272637@collegemv.qc.ca>
2024-02-07 16:00:14 -0500

    Ajout de la page de taches

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
-function recup(){
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
-    localStorage.setItem(user)
-    alert("user")
+//function recup(){
+  //  let user = document.getElementById("username").value;
+ ////  localStorage.setItem("user", user);
+  //  localStorage.setItem("pw",pass);
+//}
+
+const taskInput = document.getElementById("taskInput") ;
+const addTaskBtn = document.getElementById("addTaskBtn") ;
+const taskList = document.getElementById("taskList") ;
+
+addTaskBtn.addEventListener("click", addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    // La fonction trim() en JavaScript est utilisée pour supprimer les espaces (espaces, tabulations et
+  //  nouvelles lignes) au début et à la fin d'une chaîne.
+
+    if (taskText !== ""){
+        const listItem = document.createElement("li")
+        listItem.textContent = taskText;
+        taskList.appendChild(listItem);
+        taskInput.value = "";
+
+        // Ajoutez un bouton de suppression à chaque élément de liste
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Supprimer";
+
+        // La méthode JavaScript appendChild() est utilisée pour insérer un nouveau nœud ou repositionner un
+        //nœud existant en tant que dernier enfant d'un nœud parent particulier.
+
+        listItem.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener("click", () => {
+        listItem.remove();
+        });
+        } 
+    else {
+        alert("Veuillez entrer une tâche valide.");
+    }
+}
+
+function markTask(){
+const element =   document.replaceChild("","Terminer la tâche")
+
 }
